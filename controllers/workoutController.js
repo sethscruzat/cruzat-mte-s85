90fbf5ff7633627c704ba2e4ed70cace8834286f
sethscruzat <sethscruzat@gmail.com>
2026-08-07 10:05:05 +0800

    added baseline requirements for workout routes and controllers

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,1 +1,69 @@
 const Workout = require("../models/Workout.js");
+const {errorHandler} = require("../middleware/auth.js")
+
+module.exports.addWorkout = async (req, res) =>{
+    const {name, duration} = req.body
+    try{
+
+        if(!duration.includes("minutes".toLowerCase()) || 
+        duration.includes("seconds".toLowerCase()) ||
+        duration.includes("hours".toLowerCase())){
+            return res.status(400).send({error: "Duration must include proper duration."})
+        }
+
+        const newWorkout = new Workout({
+            userId: req.user.id,
+            name: name,
+            duration: duration.toLowerCase()
+        })
+
+        const response = await newWorkout.save();
+
+        if(!response){
+            return res.status(400).send({error: "Error when adding workout."})
+        }
+
+        return res.status(201).send(response)
+    }catch(error){
+        errorHandler(error, req, res);
+    }
+}
+
+module.exports.getMyWorkouts = async (req, res) =>{
+    try{
+        const response = await Workout.find({userId: req.user.id})
+
+        if(!response){
+            return res.status(404).send({message: "No workouts found."})
+        }
+
+        return res.status(200).send({workouts: response})
+
+    }catch(error){
+        errorHandler(error, req, res);
+    }
+}
+
+module.exports.updateWorkout = async (req, res) =>{
+    try{
+
+    }catch(error){
+        errorHandler(error, req, res);
+    }
+}
+
+module.exports.deleteWorkout = async (req, res) =>{
+    try{
+
+    }catch(error){
+        errorHandler(error, req, res);
+    }
+}
+
+module.exports.completeWorkoutStatus = async (req, res) =>{
+    try{
+
+    }catch(error){
+        errorHandler(error, req, res);
+    }
+}
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const { verify } = require("../middleware/auth.js");
+const workoutController = require("../controllers/workoutController.js")
 
+router.post("/addWorkout", verify, workoutController.addWorkout);
 
+router.get("/getMyWorkouts", verify, workoutController.getMyWorkouts);
 
+router.patch("/updateWorkout", verify, workoutController.updateWorkout);
 
+router.delete("/deleteWorkout", verify, workoutController.deleteWorkout);
 
+router.patch("/completeWorkoutStatus", verify, workoutController.completeWorkoutStatus);
 
 module.exports = router;
